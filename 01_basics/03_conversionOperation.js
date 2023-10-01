eb3f0a29b0a9bfc9bdedab3e74e9cf91c7818c4a
moSarupAli <76951266+moSarupAli@users.noreply.github.com>
2023-10-01 19:42:30 +0000

    operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -26,5 +26,15 @@ let booleanIsLoggedIn = Boolean(isLoggedIn);
 let someNumber = 33;
 
 let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// ********* Operations **********
+
+let value = 3;
+
+let negValue = -value;
+// console.log(negValue);
+
+console.log(2);
