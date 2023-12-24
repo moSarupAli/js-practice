df79ca9319bca45996f2a9a09f32497c25ffb447
moSarupAli <76951266+moSarupAli@users.noreply.github.com>
2023-12-24 13:13:15 +0000

    datatypes summery

---
+++ 01_basics/041_datatypes-summery.js
@@ -0,0 +1,38 @@
+// Primitive
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100;
+const scoreValue = 100.3;
+
+const isLoggedIn = false;
+const outsideTemp = null; // null is null, it is completely empty, it isn't zero or undefined
+let userEmail; // same as " let userEmail = undefined; "
+
+const id = Symbol('123');
+const anotherId = Symbol('123');
+console.log(id === anotherId); // false
+
+const bigNumber = 36987542123568256325478932514978321n; // n at end for BigInt (Big Integer)
+
+
+
+// Reference (Non primitive)
+
+// 3 types: Array, Objects, Functions
+
+const heros = ["srk", "Uttam Kumar", "Batman"];
+
+let myObj = {
+    name: "Sarup",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello World");
+}
+
+
+console.log(typeof scoreValue, "\n", typeof bigNumber, "\n", typeof id, "\n", typeof outsideTemp);
+console.log("-----");
+console.log(typeof heros, "\n", typeof myObj, "\n", typeof myFunction);
