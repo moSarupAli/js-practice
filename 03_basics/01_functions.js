0c9c4013ee044b5fb9ca1035b18fb6eef871eb26
moSarupAli <76951266+moSarupAli@users.noreply.github.com>
2023-12-27 18:56:20 +0000

    functions and parameters

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -8,3 +8,36 @@ function sayMyName() {
 }
 
 // sayMyName();
+
+
+// function addTwoNumbers(number1, number2){
+//     console.log(number1 + number2);
+// }
+
+function addTwoNumbers(number1, number2){
+    // let result = number1 + number2;
+    // return result;
+    return number1 + number2;
+}
+
+// addTwoNumbers(3, 4); // 7
+// addTwoNumbers(3, "4"); // 34
+// addTwoNumbers(3, "a"); // 3a
+
+const result = addTwoNumbers(3, 5);
+// console.log("Result: ", result);
+
+
+
+
+function loginUserMessage(username){
+    if(username === undefined){
+        console.log("Please enter a valid username");
+        return;
+    }
+    return `${username} just logged in`;
+}
+
+// console.log(loginUserMessage("sarup"));
+// console.log(loginUserMessage(""));
+console.log(loginUserMessage());
