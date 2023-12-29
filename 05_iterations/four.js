5f7080c7f7816d7ec055bf975345b45bf7c7352c
moSarupAli <76951266+moSarupAli@users.noreply.github.com>
2023-12-29 14:14:05 +0000

    for in loop

---
+++ 05_iterations/four.js
@@ -0,0 +1,32 @@
+const myObject = {
+    js: 'javascript',
+    cpp: 'C++',
+    rb: 'Ruby',
+    swift: 'swift by apple'
+}
+
+
+for (const key in myObject) {
+    // console.log(`"${key}" is shortcut for "${myObject[key]}"`);
+}
+
+
+const arr = [9, 8, 3, 4, 5];
+
+for (const key in arr){
+    // console.log(`key is ${key} and value is ${arr[key]}`);
+}
+
+
+
+
+// const myMap = new Map();
+
+// myMap.set('IN', "India");
+// myMap.set('USA', "United States of America");
+// myMap.set('Fr', "France");
+// myMap.set('IN', "India");
+
+// for (const key in myMap) {
+//     console.log(key); // no output as Map is "not iterable in this way"
+//  }
